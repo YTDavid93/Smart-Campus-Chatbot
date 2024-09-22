c54b95037a5f9678bbf3c73080cec273f4089611
YTDavid93 <ytd3809@gmail.com>
2024-09-22 13:19:02 +0545

    wrap app inside authprovider and load toast

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { AuthProvider } from "./context/AuthProvider.tsx";
+import { Toast } from "./utils/Toast.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider>
+      <App />
+      <Toast />
+    </AuthProvider>
+  </StrictMode>
+);
