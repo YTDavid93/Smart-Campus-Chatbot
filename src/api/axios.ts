4b59add045f62936063080964b8519335c506a3e
YTDavid93 <ytd3809@gmail.com>
2024-09-23 12:05:12 +0545

    build axiosinstance to set accesstoken in headers

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,24 @@
 import axios from "axios";
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("chatbot-token"); 
+    if (token) {
+      config.headers["x-auth-token"] = `${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
