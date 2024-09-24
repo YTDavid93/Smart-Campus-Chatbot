867a7474859009ec4d5952aef115af1f6e592306
YTDavid93 <ytd3809@gmail.com>
2024-09-24 12:16:26 +0545

    code refactor

--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -4,9 +4,14 @@ import MainPage from "./Body/MainPage"; // Assuming MainPage contains the logo o
 import ChatInput from "./ChatInput/ChatInput";
 import NavBar from "./Header/NavBar";
 import useAuth from "../hooks/useAuth";
+import { CircleLoader } from "../utils/CircleLoader";
+import ErrorMessage from "../utils/ErrorMessage";
+import { MessageBubble } from "./Message/MessageBubble";
+
 
 const DashboardPage = () => {
-  const { sendMessage, messages } = useHandleQuery();
+
+  const { sendMessage, messages, loading, error } = useHandleQuery();
   const navigate = useNavigate();
   const { setAuth } = useAuth();
 
@@ -16,39 +21,35 @@ const DashboardPage = () => {
     navigate("/login");
   };
 
+
   return (
     <>
-      <NavBar onClick={handleSignOut} />
-
-      <div className="flex flex-col justify-between h-screen p-6">
-        {messages.length === 0 ? (
-          <div className="flex flex-1 items-center justify-center">
-            <MainPage />
-          </div>
+      <>
+        <NavBar onClick={handleSignOut} />
+        {loading ? (
+          <CircleLoader />
         ) : (
-          <div className="flex-1 overflow-y-auto mb-4">
-            <div>
-              {messages.map((msg, index) => (
-                <div key={index} className="mb-4">
-                  <div className="flex justify-end mb-2">
-                    <div className="relative max-w-[70%] rounded-3xl px-4 py-2 bg-blue-500 text-white self-end">
-                      {msg.question}
-                    </div>
-                  </div>
-
-                  <div className="flex justify-start">
-                    <div className="relative max-w-[70%] rounded-3xl px-4 py-2 bg-gray-300 text-black self-start">
-                      {msg.text}
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
+          <div className="flex flex-col justify-between h-screen p-6">
+            {error && <ErrorMessage>{error}</ErrorMessage>}
+            {messages.length === 0 ? (
+              <div className="flex flex-1 items-center justify-center">
+                <MainPage />
+              </div>
+            ) : (
+              <div className="flex-1 overflow-y-auto mb-4">
+                {messages.map((msg) => (
+                  <MessageBubble
+                    key={msg.id}
+                    question={msg.question}
+                    response={msg.response}
+                  />
+                ))}
+              </div>
+            )}
+            <ChatInput onClick={sendMessage} />
           </div>
         )}
-
-        <ChatInput onClick={sendMessage} />
-      </div>
+      </>
     </>
   );
 };
