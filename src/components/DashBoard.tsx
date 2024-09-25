e8f04ef5efc8a1c5dc64f8586805416d4b4647ae
YTDavid93 <ytd3809@gmail.com>
2024-09-25 11:08:17 +0545

    change id to _id

--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import useHandleQuery from "../hooks/useHandleQuery";
-import MainPage from "./Body/MainPage"; // Assuming MainPage contains the logo or welcome screen
+import MainPage from "./Body/MainPage";
 import ChatInput from "./ChatInput/ChatInput";
 import NavBar from "./Header/NavBar";
 import useAuth from "../hooks/useAuth";
@@ -10,7 +10,6 @@ import { MessageBubble } from "./Message/MessageBubble";
 
 
 const DashboardPage = () => {
-
   const { sendMessage, messages, loading, error } = useHandleQuery();
   const navigate = useNavigate();
   const { setAuth } = useAuth();
@@ -21,35 +20,32 @@ const DashboardPage = () => {
     navigate("/login");
   };
 
-
   return (
     <>
-      <>
-        <NavBar onClick={handleSignOut} />
-        {loading ? (
-          <CircleLoader />
-        ) : (
-          <div className="flex flex-col justify-between h-screen p-6">
-            {error && <ErrorMessage>{error}</ErrorMessage>}
-            {messages.length === 0 ? (
-              <div className="flex flex-1 items-center justify-center">
-                <MainPage />
-              </div>
-            ) : (
-              <div className="flex-1 overflow-y-auto mb-4">
-                {messages.map((msg) => (
-                  <MessageBubble
-                    key={msg.id}
-                    question={msg.question}
-                    response={msg.response}
-                  />
-                ))}
-              </div>
-            )}
-            <ChatInput onClick={sendMessage} />
-          </div>
-        )}
-      </>
+      <NavBar onClick={handleSignOut} />
+      {loading ? (
+        <CircleLoader />
+      ) : (
+        <div className="flex flex-col justify-between h-screen overflow-y-auto p-6">
+          {error && <ErrorMessage>{error}</ErrorMessage>}
+          {messages.length === 0 ? (
+            <div className="flex flex-1 items-center justify-center">
+              <MainPage />
+            </div>
+          ) : (
+            <div className="flex-1 mb-4">
+              {messages.map((msg) => (
+                <MessageBubble
+                  key={msg._id}
+                  question={msg.question}
+                  response={msg.response}
+                />
+              ))}
+            </div>
+          )}
+          <ChatInput onClick={sendMessage} />
+        </div>
+      )}
     </>
   );
 };
