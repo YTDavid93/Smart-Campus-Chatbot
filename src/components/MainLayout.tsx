6859ab4139e66fb0241d9d93914eb390bb902778
YTDavid93 <ytd3809@gmail.com>
2024-09-25 11:09:26 +0545

    build main layout component

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,9 +1,30 @@
 import { Outlet } from "react-router-dom";
+import SideBar from "./Header/SideBar";
+import useHandleQuery from "@/hooks/useHandleQuery";
+import { useEffect, useState } from "react";
 
 const MainLayout = () => {
+  const { messages, setMessages, fetchMessages } = useHandleQuery();
+
+  const [newconversation, setNewConversation] = useState(false);
+
+  useEffect(() => {
+    if (!newconversation) {
+      fetchMessages();
+    }
+  }, [newconversation]);
+
+
+  const handleClick = () => {
+    setMessages([]);
+    console.log(messages);
+    setNewConversation(true)
+  };
+
   return (
-    <div>
-      <main className="flex flex-col h-screen">
+    <div className="flex">
+      <SideBar onClick={handleClick} />
+      <main className="flex flex-1 flex-col h-screen">
         <Outlet />
       </main>
     </div>
