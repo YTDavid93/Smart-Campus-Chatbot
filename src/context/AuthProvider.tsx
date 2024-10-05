a71abba65b042dc7d795f5d6731851b4ecc53f05
YTDavid93 <ytd3809@gmail.com>
2024-10-05 13:15:14 +0545

    conversations, conversationTitle and title as a global auth

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,3 +1,4 @@
+import { Conversation } from "@/hooks/useUserQuery";
 import { createContext, ReactNode, useState, useEffect } from "react";
 
 interface Props {
@@ -13,12 +14,21 @@ interface Auth {
 interface AuthContextType {
   auth: Auth;
   setAuth: (auth: Auth) => void;
+  conversations: Conversation | null;
+  setConversations: (conversations: Conversation | null) => void;
+  title: string;
+  setTitle: (title: string) => void;
+  conversationTitle: Conversation[];
+  setConversationTitle: (conversations: Conversation[]) => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: Props) => {
   const [auth, setAuth] = useState<AuthContextType["auth"]>({});
+  const [conversations, setConversations] = useState<Conversation | null>(null);
+  const [conversationTitle, setConversationTitle] = useState<Conversation[]>([]);
+  const [title, setTitle] = useState("");
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -39,7 +49,18 @@ export const AuthProvider = ({ children }: Props) => {
   }
 
   return (
-    <AuthContext.Provider value={{ auth, setAuth }}>
+    <AuthContext.Provider
+      value={{
+        auth,
+        setAuth,
+        conversations,
+        setConversations,
+        title,
+        setTitle,
+        conversationTitle,
+        setConversationTitle
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
