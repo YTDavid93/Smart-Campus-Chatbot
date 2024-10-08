f175653e830d5578e686d18bfb725132d467a8bb
YTDavid93 <ytd3809@gmail.com>
2024-10-08 23:49:21 +0545

    threedots ui change

--- src/components/Header/SideBar.tsx
+++ src/components/Header/SideBar.tsx
@@ -4,18 +4,20 @@ import { useNavigate } from "react-router-dom";
 import { Loader } from "@/utils/Loading";
 import ErrorMessage from "@/utils/ErrorMessage";
 import useAuth from "@/hooks/useAuth";
+import { BsThreeDots } from "react-icons/bs";
+import { useState } from "react";
+import ShowPopover from "../ui/ShowPopover";
 
 const SideBar = () => {
-  const {
-    conversationTitle,
-    loading,
-    error,
-    fetchUserConversation,
-  } = useGetQuery();
-
+  const { conversationTitle, loading, error, fetchUserConversation } =
+    useGetQuery();
   const navigate = useNavigate();
-
   const { setConversations, conversations } = useAuth();
+
+  const [isPopoverOpen, setIsPopoverOpen] = useState<boolean>(false);
+  const [hoveredConversationId, setHoveredConversationId] = useState<
+    string | null
+  >(null);
 
   const handleTitleClick = (
     conversationId: string,
@@ -23,7 +25,6 @@ const SideBar = () => {
   ) => {
     fetchUserConversation(conversationId);
     navigate(`/conversations/${conversationId}`);
-
     localStorage.setItem("currentConversationId", conversationId);
     localStorage.setItem("currentConversationTitle", conversationTitle);
   };
@@ -48,28 +49,44 @@ const SideBar = () => {
       {loading && <Loader />}
       {error && <ErrorMessage>{error}</ErrorMessage>}
 
-      {conversations && (
-        <div
-          key={conversations._id}
-          className="cursor-pointer font-bold"
-          onClick={() =>
-            handleTitleClick(conversations._id, conversations.title)
-          }
-        >
-          {conversations.title}
-        </div>
-      )}
-      
-      {filteredTitle &&
-        filteredTitle.map((conv) => (
+      <div className="flex flex-col gap-4 mt-5 relative">
+        {conversations && (
           <div
-            key={conv._id}
-            className="cursor-pointer"
-            onClick={() => handleTitleClick(conv._id, conv.title)}
+            key={conversations._id}
+            className="cursor-pointer bg-slate-300 rounded-md p-2 flex items-center justify-between"
+            onClick={() =>
+              handleTitleClick(conversations._id, conversations.title)
+            }
           >
-            {conv.title}
+            <span>{conversations.title}</span>
+            <BsThreeDots onClick={() => setIsPopoverOpen(true)} />
           </div>
-        ))}
+        )}
+
+        {filteredTitle &&
+          filteredTitle.map((conv) => (
+            <div
+              key={conv._id}
+              className="cursor-pointer hover:bg-slate-300 transition-transform duration-200 ease-in-out transform hover:scale-105 p-2 rounded-md flex justify-between items-center"
+              onMouseEnter={() => setHoveredConversationId(conv._id)}
+              onMouseLeave={() => setHoveredConversationId(null)}
+              onClick={() => handleTitleClick(conv._id, conv.title)}
+            >
+              <span>{conv.title}</span>
+
+              {hoveredConversationId === conv._id && (
+                <BsThreeDots onClick={() => setIsPopoverOpen(true)} />
+              )}
+            </div>
+          ))}
+
+        <div className="absolute right-[10px] top-[10px]">
+          <ShowPopover
+            isOpen={isPopoverOpen}
+            onClose={() => setIsPopoverOpen(false)}
+          />
+        </div>
+      </div>
     </div>
   );
 };
