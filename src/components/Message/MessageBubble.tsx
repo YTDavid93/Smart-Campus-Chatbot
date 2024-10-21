aa82651d18da57c9e4b164628d506ee6bdef84aa
YTDavid93 <ytd3809@gmail.com>
2024-10-21 08:47:16 +0545

    add logo in bot response

--- src/components/Message/MessageBubble.tsx
+++ src/components/Message/MessageBubble.tsx
@@ -1,10 +1,9 @@
-
 interface Props {
-    question: string;
-    response: string   
+  question: string;
+  response: string;
 }
 
-export const MessageBubble = ({ question, response}: Props) => {
+export const MessageBubble = ({ question, response }: Props) => {
   return (
     <div className="mb-4">
       <div className="flex justify-end mb-2">
@@ -13,11 +12,16 @@ export const MessageBubble = ({ question, response}: Props) => {
         </div>
       </div>
 
-      <div className="flex justify-start">
+      <div className="flex justify-start items-center gap-3">
+        <img
+          src="/src/assets/Thames-Logo.webp"
+          alt="logo"
+          className="w-[30px] h-[30px] rounded-[15px] border border-slate-200 p-[2px] "
+        />
         <div className="relative max-w-[70%] rounded-3xl px-4 py-2 bg-gray-300 text-black self-start">
           {response}
         </div>
       </div>
     </div>
   );
-}
+};
