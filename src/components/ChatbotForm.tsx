46da750f5aef99f516411be9f00f427d60c535ea
YTDavid93 <ytd3809@gmail.com>
2024-10-23 10:17:01 +0545

    ui fixed

--- src/components/ChatbotForm.tsx
+++ src/components/ChatbotForm.tsx
@@ -8,21 +8,23 @@ const ChatbotForm = () => {
 
   return (
     <>
-      {newMessages.length === 0 ? (
-        <div className="flex flex-1 items-center justify-center">
-          <MainPage />
-        </div>
-      ) : (
-        <div className="flex-1 mb-4">
-          {newMessages.map((msg) => (
-            <MessageBubble
-              key={msg._id}
-              question={msg.question}
-              response={msg.response}
-            />
-          ))}
-        </div>
-      )}
+      <div className="flex flex-col justify-between h-screen overflow-y-auto">
+        {newMessages.length === 0 ? (
+          <div className="flex flex-1 items-center justify-center">
+            <MainPage />
+          </div>
+        ) : (
+          <div className="flex-1 mb-4">
+            {newMessages.map((msg) => (
+              <MessageBubble
+                key={msg._id}
+                question={msg.question}
+                response={msg.response}
+              />
+            ))}
+          </div>
+        )}
+      </div>
       <ChatInput />
     </>
   );
